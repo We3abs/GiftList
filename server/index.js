4e33c20e21a37bd077717aca6d055a07880c339c
Unreal <t.babinos@gmail.com>
2023-03-27 16:57:54 +0300

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+// TODO: hardcode a merkle root here representing the whole nice list
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
+
+app.post('/gift', (req, res) => {
+  // grab the parameters from the front-end here
+  const { proof, name, root } = req.body;
+
+  // TODO: prove that a name is in the list 
+  // 1. verify the proof against the merkle root
+  const isInTheList = verifyProof(proof, name, root);
+  
+  // 2. if the proof is valid, send back a toy robot
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
+
+
